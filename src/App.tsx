e7a51111ca0779a1e967449e4c18bff5d042ea17
David Valdiviezo <hvaldiviezos@unprg.edu.pe>
2025-07-29 20:43:53 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+// import { Button } from "@/components/ui/button";
+// import { ThemeProvider } from "./components/theme-provider";
+// import { ModeToggle } from "./components/mode-toggle";
+
+// function App() {
+//   return (
+//     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+//       <div className="flex min-h-svh flex-col items-center justify-center">
+//         <Button>Iniciar sesión</Button>
+//       </div>
+//       <ModeToggle />
+//     </ThemeProvider>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import type { JSX } from "react";
+import HomePage from "./pages/home/components/HomePage";
+import LayoutComponent from "./components/layout";
+import { ThemeProvider } from "./components/theme-provider";
+
+function ProtectedRoute({ children }: { children: JSX.Element }) {
+  // const { token } = useAuthStore();
+  // if (!token) {
+  //   return <Navigate to="/login" replace />;
+  // }
+
+  return <LayoutComponent>{children}</LayoutComponent>;
+}
+export default function App() {
+  // const { token } = useAuthStore();
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          {/* Ruta pública */}
+          {/* <Route
+            path="/login"
+            element={token ? <Navigate to="/inicio" /> : <LoginPage />}
+          /> */}
+
+          {/* Ruta protegida */}
+          {/* <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <LoginPage />
+              </ProtectedRoute>
+            }
+          /> */}
+
+          <Route
+            path="/inicio"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* 404 */}
+          <Route path="*" element={<Navigate to="/inicio" />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
---
+++ src/components/team-switcher.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Command } from "lucide-react";
+
+import {
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+export function TeamSwitcher() {
+  return (
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <SidebarMenuButton size="lg" asChild>
+            <a href="#">
+              {/* <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <Command className="size-4" />
+              </div> */}
+              <div className="hidden md:grid flex-1 place-items-center text-left text-sm leading-tight">
+                <span className="truncate font-bold text-primary">Mr. Soft</span>
+                <span className="truncate text-xs">ERP System</span>
+              </div>
+            </a>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
+  );
+}
